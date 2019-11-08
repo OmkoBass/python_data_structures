128593b0de08796e8760efe7cdb8d0956546d2a4
OmkoBass <omkob@hotmail.com>
2019-11-08 15:59:05 -0800

    python_data_structures
    Cleaned up binary search tree

--- Trees/binary_search_tree.py
+++ Trees/binary_search_tree.py
@@ -7,19 +7,6 @@ class node:
     def __str__(self):
         return str(self.value)
 
-    def add(self, newnode):
-        if self is not None:
-            if newnode.value < self.value:
-                if self.left is not None:
-                    self.left.add(newnode)
-                elif self.left is None:
-                    self.left = newnode
-            elif newnode.value > self.value:
-                if self.right is not None:
-                    self.right.add(newnode)
-                elif self.right is None:
-                    self.right = newnode
-
     def preorder(self):
         print(f"{self.value} ", end = "")
         if self.left:
@@ -27,7 +14,62 @@ class node:
         if self.right:
             self.right.preorder()
 
-root = node(16)
-root.add(node(8))
-root.add(node(22))
-root.preorder()
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        print(f"{self.value} ", end = "")
+        if self.right:
+            self.right.inorder()
+        
+    def postorder(self):
+        if self.left:
+            self.left.postorder()
+        if self.right:
+            self.right.postorder()
+        print(f"{self.value} ", end = "")
+    
+
+class binarytree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, newnode):
+        if newnode is not node:
+            node(newnode)
+
+        if self.root is None:
+            self.root = newnode
+            return
+        
+        if newnode.value < self.root.value:
+            if self.root.left is not None:
+                self.root.left.add(newnode)
+            elif self.root.left is None:
+                self.root.left = newnode
+        elif newnode.value > self.root.value:
+            if self.root.right is not None:
+                self.root.right.add(newnode)
+            elif self.root.right is None:
+                self.root.right = newnode    
+
+    def preorder(self):
+        self.root.preorder()
+
+    def inorder(self):
+        self.root.inorder()
+
+    def postorder(self):
+        self.root.postorder()
+    
+    def tree_sum(self):
+        self.root.tree_sum()        
+
+mytree = binarytree()
+
+mytree.add(node(16))
+mytree.add(node(8))
+mytree.add(node(22))
+
+mytree.inorder()
+
+print(mytree.tree_sum())
